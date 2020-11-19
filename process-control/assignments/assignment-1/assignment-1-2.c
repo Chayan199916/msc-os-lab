28306409326a2bb1214cdaab98aec598e0d005f9
root <swapnomoy199916@gmail.com>
2020-11-19 17:16:21 -0500

    imp notes added

--- process-control/assignments/assignment-1/assignment-1-2.c
+++ process-control/assignments/assignment-1/assignment-1-2.c
@@ -17,8 +17,8 @@ int main(){
 
         printf("Entered in child process. pid : %d ppid : %d\n", getpid(), getppid());
         execv("./assignment-1-1", args);
-        perror("Failed to replace process image\n");
-        _exit(errno);
+        perror("Failed to replace process image\n"); // this line gets executed means execv failed. no need to check seperately.
+        _exit(errno); //inside child process, it's better to use _exit() coz it doesn't flush out i/o buffers whereas exit() does.
 
     }
     else if(pid == -1){
