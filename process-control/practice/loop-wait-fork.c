c544b6131a34e440ea80024d7363bfc5220af66c
root <swapnomoy199916@gmail.com>
2020-11-24 05:20:46 -0500

    wait loop and fork related class work done

---
+++ process-control/practice/loop-wait-fork.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<sys/types.h>
+#include<stdlib.h>
+#include<sys/errno.h>
+#include<sys/wait.h>
+
+int main(int argc, char *argv[]){
+
+    pid_t pid;
+    int n, i, status;
+    
+    pid = fork();
+
+    if (pid == -1)
+    {
+        
+        perror("Fork call fails");
+        exit(errno);
+        
+    }else /*parent and child process handling*/
+    {
+
+        wait(&status);
+        printf("Enter the value of n : ");
+        scanf("%d", &n);
+        for (i = 0; i < n; i++)
+            printf("%d", i);
+
+    }
+
+}
